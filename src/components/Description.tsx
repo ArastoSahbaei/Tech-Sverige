cc243727e62819d44f20bf42712bbbe545a1cfa2
arasto <Arasto.Sahbaei@gmail.com>
2021-05-28 13:00:16 +0200

    m

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -2,13 +2,13 @@ export const Description = () => {
 	return (
 		<div className='descriptionWrapper'>
 			<p>Svetech.io</p>
-			<span>Vi är utvecklare precis som du! <br /> Gå med i vår Discord och känn dig som hemma.</span>
+			<p className='subParagraph'>Vi är utvecklare precis som du! <br /> Gå med i vår Discord och känn dig som hemma.</p>
 
 			<p>Få insikter</p>
-			<span>Möt likasinnade entusiaster, be om hjälp och få din kod granskad. Vi är här för varandra.</span>
+			<p className='subParagraph'>Möt likasinnade entusiaster, be om hjälp och få din kod granskad. Vi är här för varandra.</p>
 
-			<p>Inspirera samt bli inspirerad</p> 
-			<span>Jämför lösningar och lär dig från de bästa programmerarna.</span>
+			<p>Inspirera samt bli inspirerad</p>
+			<p className='subParagraph'>Jämför lösningar och lär dig från de bästa programmerarna.</p>
 		</div>
 	)
 }

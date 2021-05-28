24af7da529320c3619564da52b5dc1ad14e615ae
arasto <Arasto.Sahbaei@gmail.com>
2021-05-28 10:40:13 +0200

    m

---
+++ src/components/Description.tsx
@@ -0,0 +1,15 @@
+export const Description = () => {
+	return (
+		<div className='descriptionWrapper'>
+			<p>Svetech.io</p> <br />
+			<span>Vi är utvecklare precis som du! <br/> Gå med i vår Discord och känn dig som hemma.</span>
+			<p>Få insikter</p> <br />
+			<span>Möt likasinnade entusiaster, be om hjälp och få din kod granskad. Vi är här för varandra.</span>
+			<p>Inspirera samt bli inspirerad</p> <br />
+			<span>Jämför lösningar och lär dig från de bästa programmerarna.</span>
+		</div>
+	)
+}
+
+
+ 
---
+++ src/components/Discord.tsx
@@ -0,0 +1,13 @@
+export const Discord = () => {
+	return (
+		<div className='discordWrapper'>
+			<iframe
+				className='iFrameDiscord'
+				title='tech sverige'
+				src="https://discord.com/widget?id=769244240307814431&theme=dark"
+				allowTransparency={true}
+				frameBorder={0}
+				sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts" />
+		</div>
+	)
+}
